8a55d903f1c3c75544dac265063f8b2f6af21d84
FabianFernandez19 <145621184+FabianFernandez19@users.noreply.github.com>
2024-03-22 09:26:20 -0500

    fix

--- app/Models/mascota_has_logros.php
+++ app/Models/mascota_has_logros.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use App\Models\Logros;
+use App\Models\logros;
 use App\Models\Informacion;
 
 
